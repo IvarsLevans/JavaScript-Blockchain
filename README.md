45cdab82554671decb1519b3d6131174f6aafee1
IvarsLevans <ivars.levans@gmail.com>
2021-06-23 00:46:23 +0300

    Signing transactions

--- README.md
+++
@@ -1,3 +0,0 @@
-# JavaScript Blockchain
-
-## Source: https://www.youtube.com/playlist?list=PLzvRQMJ9HDiTqZmbtFisdXFxul5k0F-Q4
--- main.js
+++ main.js
@@ -1,111 +1,19 @@
-const SHA256 = require("crypto-js/sha256");
+const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-class Transaction {
-    constructor(fromAddress, toAddress, amount) {
-        this.fromAddress = fromAddress;
-        this.toAddress = toAddress;
-        this.amount = amount;
-    }
-}
+const myKey = ec.keyFromPrivate("7f660fb0d0a500de2ece0f151c43aa1168b28d9144cfd489430818a636dba586");
+const myWalletAddress = myKey.getPublic('hex');
 
-class Block {
-    constructor(timestamp, transactions, previousHash = "") {
-        this.timestamp = timestamp;
-        this.transactions = transactions;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
-    }
+const Metrona = new Blockchain();
 
-    calculateHash() {
-        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
-    }
-
-    mineBlock(difficulty) {
-        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-        console.log(" BLOCK MINED: " + this.hash);
-    }
-}
-
-class Blockchain {
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 4;
-        this.pendingTransactions = [];
-        this.miningReward = 100;
-    }
-
-    createGenesisBlock() {
-        return new Block("01/01/2021", "Genesis block", "0");
-    }
-
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    minePendingTransactions(miningRewardAddress) {
-        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
-        block.mineBlock(this.difficulty);
-
-        console.log("  Block successfuly mined!");
-        this.chain.push(block);
-
-        this.pendingTransactions = [
-            new Transaction(null, miningRewardAddress, this.miningReward)
-        ];
-    }
-
-    createTransaction(transaction) {
-        this.pendingTransactions.push(transaction);
-    }
-
-    getBalanceOfAddress(address) {
-        let balance = 0;
-        for (const block of this.chain) {
-            for (const trans of block.transactions) {
-                if (trans.fromAddress === address) {
-                    balance -= trans.amount;
-                }
-                if (trans.toAddress === address) {
-                    balance += trans.amount;
-                }
-            }
-        }
-        return balance;
-    }
-
-    isChainValid() {
-        for (let i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-                return false;
-            }
-
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-let Metrona = new Blockchain();
-
-Metrona.createTransaction(new Transaction("address1", "address2", 100));
-Metrona.createTransaction(new Transaction("address2", "address1", 50));
+const tx1 = new Transaction(myWalletAddress, 'address2', 10);
+tx1.signTransaction(myKey);
+Metrona.addTransaction(tx1);
 
 console.log("\nStarting the miner...");
-Metrona.minePendingTransactions("miners-address");
+Metrona.minePendingTransactions(myWalletAddress);
 
-console.log("\nBalance of miner is", Metrona.getBalanceOfAddress("miners-address"));
+console.log("\nBalance of miner is", Metrona.getBalanceOfAddress(myWalletAddress));
 
-console.log("\nStarting the miner...");
-Metrona.minePendingTransactions("miners-address");
-
-console.log("\nBalance of miner is", Metrona.getBalanceOfAddress("miners-address"));
+console.log('Blockchain valid?', Metrona.isChainValid() ? 'Yes' : 'No');
